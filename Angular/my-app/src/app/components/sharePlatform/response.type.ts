53bb10c84ccfe64406528b6cc4ce9e8e690acc46
舒运凯 <2871686782@qq.com>
2018-07-04 20:02:28 +0800

    初步实现平台首页设计

---
+++ Angular/my-app/src/app/components/sharePlatform/response.type.ts
@@ -0,0 +1,6 @@
+//为后台接口的返回数据指定类型
+export class ResponseType {
+    code: string;
+    data: Object[];
+    message: string;
+}
--- Angular/my-app/src/app/components/sharePlatform/sharePlatform.component.ts
+++ Angular/my-app/src/app/components/sharePlatform/sharePlatform.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';//http请求需要用到
+import { ResponseType } from './response.type';//接口返回类型
 
 @Component({
     selector: 'share-platform',
@@ -6,8 +8,26 @@ import { Component } from '@angular/core';
     styleUrls: ['./sharePlatform.component.css']
 })
 
-export class SharePlatform {
-    constructor() {
+export class SharePlatform implements OnInit {
+    constructor(private http: HttpClient) {//依赖注入
 
+    }
+    catalogs: object[];
+    getCatalogs() {
+        var url = 'http://192.168.11.230:8080/seplatform_portal/service/homepage/deptList.do?num=30&orgId=1';
+        this.http.get<ResponseType>(url).subscribe(response => {
+            if (response.code === "20000") {
+                response.data = response.data.slice(0, response.data.length - 5);
+                for (let i in response.data) {
+                    response.data[i].style = {
+                        "background-position": "0 " + "-" + 40 * i + "px"
+                    };
+                }
+                this.catalogs = response['data'];
+            }
+        });
+    }
+    ngOnInit() {
+        this.getCatalogs();
     }
 }
