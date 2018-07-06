0413ef4597fd65a6732686c93e2522d5612a09dc
舒运凯 <2871686782@qq.com>
2018-07-06 17:20:25 +0800

    调整

--- Angular/my-app/src/app/components/sharePlatform/sharePlatform.component.ts
+++ Angular/my-app/src/app/components/sharePlatform/sharePlatform.component.ts
@@ -28,7 +28,7 @@ export class SharePlatform implements OnInit {
         });
     }
     getArticles(){
-        var url = 'http://192.168.11.230:8080/seplatform_portal/service/sharedDynamic/getNewsPage.do?pageNo=1&pageSize=6&title=&source=&author=';
+        var url = 'http://192.168.11.230:8080/seplatform_portal/service/sharedDynamic/getNewsPage.do?pageNo=1&pageSize=8&title=&source=&author=';
         this.http.get(url).subscribe((response: any) => {//这里需要把response指定为any类型，注意加上小括号，不然会进行类型检测
             if (response.code === "20000") {
                 this.articles = response.data.result;
