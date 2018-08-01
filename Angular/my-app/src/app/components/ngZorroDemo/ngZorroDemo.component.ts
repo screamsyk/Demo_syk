1b4f08a51d33873dbc88b73a3a1a580ff0a2b9a4
舒运凯 <2871686782@qq.com>
2018-08-01 18:00:27 +0800

    学习NG-ZORRO导航菜单和表格的使用，一些需求如改变列宽，通过NG-ZORRO的表格是不支持的

--- Angular/my-app/src/app/components/ngZorroDemo/ngZorroDemo.component.ts
+++ Angular/my-app/src/app/components/ngZorroDemo/ngZorroDemo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { NzDropdownContextComponent, NzDropdownService, NzMenuItemDirective } from 'ng-zorro-antd';
+import { NzDropdownContextComponent, NzDropdownService, NzMessageService } from 'ng-zorro-antd';
 
 @Component({
     selector: 'ng-zorro-demo',
@@ -8,15 +8,98 @@ import { NzDropdownContextComponent, NzDropdownService, NzMenuItemDirective } fr
 })
 
 export class ngZorroDemo {
-    private dropdown: NzDropdownContextComponent;//右键菜单
     title: string = 'NG-ZORRO学习';
+    menuType: string = 'horizontal';
+    menuTheme: string = 'light';
+    private dropdown: NzDropdownContextComponent;//右键菜单
+    dataSet = [
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+            checked: false
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+            checked: false
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+            checked: false
+        }
+    ];
+    dataSet2=this.dataSet.concat(this.dataSet);
+    allChecked: boolean = false;
+    indeterminate: boolean = false;
+    listOfSelection = [
+        {
+            text: '全选',
+            onSelect: () => {
+                this.checkAll(true);
+            }
+        },
+        {
+            text: '选中偶数行',
+            onSelect: () => {
+                this.dataSet.forEach((data, index) => data.checked = index % 2 === 0);
+                this.refreshStatus();
+            }
+        },
+        {
+            text: '选中奇数行',
+            onSelect: () => {
+                this.dataSet.forEach((data, index) => data.checked = index % 2 !== 0);
+                this.refreshStatus();
+            }
+        }
+    ]
+    sortName = null;
+    sortValue = null;
+
+    //全选
+    checkAll(value: boolean): void {
+        this.dataSet.forEach(data => data.checked = value);
+        this.allChecked = true;
+        this.indeterminate = false;
+    }
+
+    //判断是否全选
+    refreshStatus(): void {
+        this.allChecked = this.dataSet.every(data => { return data.checked });//是否都选中了。数组的every()方法：判断数组中的元素是否都满足某个条件
+        let allNotChecked = this.dataSet.every(data => { return !data.checked });//是否都没选中
+        this.indeterminate = !this.allChecked && !allNotChecked;//既有选中的又有没选中的
+    }
+
+    //排序
+    sort(event): void {
+        this.sortName = event.key;
+        this.sortValue = event.value;
+        if (this.sortName) {
+            this.dataSet = this.dataSet.sort((a, b) => (this.sortValue === 'ascend') ? (a[this.sortName] > b[this.sortName] ? 1 : -1) : (b[this.sortName] > a[this.sortName] ? 1 : -1));
+        } else {
+            this.dataSet = this.dataSet;
+        }
+    }
+
+    //创建右键菜单
     contextMenu($event: MouseEvent, template: TemplateRef<void>): void {
-        this.dropdown = this.nzDropdownService.create($event, template);//创建右键菜单
+        this.dropdown = this.nzDropdownService.create($event, template);
     }
+
+    //关闭右键菜单
     close(): void {
-        this.dropdown.close();//关闭右键菜单
+        this.dropdown.close();
     }
-    constructor(private nzDropdownService: NzDropdownService) {
+
+    //构造函数
+    constructor(private nzDropdownService: NzDropdownService, private message: NzMessageService) {
 
     }
 }
