ec6db961ea737d5e29c35ca2bba6370fc586beeb
舒运凯 <2871686782@qq.com>
2018-12-06 16:58:31 +0800

    调整页面加载进度条的显示

--- Angular/my-app/src/app/components/appRoot/app.component.ts
+++ Angular/my-app/src/app/components/appRoot/app.component.ts
@@ -11,6 +11,7 @@ import { NgProgress } from '@ngx-progressbar/core';//页面加载效果——进
 export class AppComponent implements OnInit {
   title = '我的第一个Angular应用：漫威超级英雄';
   loading$: Observable<boolean>;//可观察序列
+  timer: any;
   constructor(private pageLoadingService: PageLoadingService, private progressBar: NgProgress) {
 
   }
@@ -18,11 +19,12 @@ export class AppComponent implements OnInit {
     this.loading$ = this.pageLoadingService.loading$;
     this.loading$.subscribe(value => {
       if (value) {
+        clearTimeout(this.timer);
+        this.progressBar.set(0);
         this.progressBar.start();//开始显示加载进度条
       } else {
-        let that = this;
-        setTimeout(function () {
-          that.progressBar.complete();//结束显示
+        this.timer = setTimeout(() => {
+          this.progressBar.complete();//结束显示
         }, 3000)
       }
     });//订阅主题
