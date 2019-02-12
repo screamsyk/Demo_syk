87577321d3ad8a9d71e5727f750a03180bb69566
screamsyk <2871686782@qq.com>
2019-02-12 22:33:08 +0800

    微信小程序页面

--- WeChat/learn.js
+++ WeChat/learn.js
@@ -61,9 +61,46 @@
 
 //(1)app.js中执行App()函数来注册一个小程序。参数为生命周期监测对象
 App({
-    onLaunch(options) { },//小程序初始化
+    globalData: 1,//在APP中设置全局数据
+    onLaunch(options) {//小程序初始化，即启动
+        options = wx.getLaunchOptionsSync()
+    },
     onShow(options) { },//小程序显示
     onHide() { },//小程序隐藏
     onError(msg) { },//小程序发生错误
     onPageNotFound() { },//页面不存在
 })
+getApp();//这是全局函数，用于获取app实例，另外在App()方法中用this指代app实例。通过这个函数，页面可以得到app实例，从而得到其中的全局数据
+
+//(2)小程序中有个场景值的概念，用于重新返回小程序时，能回到对应的场景。
+//---可以在 App 的 onLaunch 和 onShow，或wx.getLaunchOptionsSync 中获取上述场景值。
+
+
+//--------------------page.js-----------------------
+
+//(1)page.js中执行Page()函数来注册一个小程序页面。
+Page({
+    data: {},//页面的初始数据
+    onLoad() { },//页面加载，可以获取页面参数
+    onShow() { },//页面显示
+    onReady() { },//页面初次渲染完成
+    onHide() { },//页面隐藏
+    onUnload() { },//页面卸载
+    onPullDownRefresh() { },//用户下拉刷新
+    onReachBottom() { },//页面上拉触底
+    onShareAppMessage() { },//用户点击右上角转发
+    onPageScroll() { },//页面滚动
+    onResize() { },//页面尺寸改变
+    onTabitemTap() { },//点击tab时
+})
+Page.prototype.setData();//设置页面数据，而不是直接修改this.data
+
+//(2)页面切换
+wx.navigateTo({ url: 'pagePath?param=value' });//打开新页面（不能是tabBar页面）
+//---等同于：<navigator open-type="navigateTo" url="pagePath?param=value"/>
+wx.redirectTo({ url: 'pagePath?param=value' });//页面重定向（不能重定向到tabBar页面）
+//---等同于：<navigator open-type="redirectTo" url="pagePath?param=value"/>
+wx.navigateBack();//返回上个页面（不能返回tabBar页面）
+//---等同于：<navigator open-type="navigateBack"/>
+wx.tabSwitch();//切换tabBar页面
+wx.reLaunch();//重启
